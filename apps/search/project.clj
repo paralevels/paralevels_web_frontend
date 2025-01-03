3a135348b698d73aa985bfebcb5163e75cee9927
Felix Zheng <felix@paralevels.com>
2025-01-03 18:48:31 -0500

    Added ClojureScript dependencies to project.clj

--- apps/search/project.clj
+++ apps/search/project.clj
@@ -4,9 +4,17 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.4"]
                  [ring "1.9.6"]
                  [compojure "1.7.0"]]
   :main ^:skip-aot search.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :cljsbuild {:builds [{:id "search"
+                        :source-paths ["src/search"]
+                        :compiler {:output-to "resources/public/js/search.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]})

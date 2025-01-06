2b2c6fe738239156960614d1637d7a1ab49ff914
Felix Zheng <felix@paralevels.com>
2025-01-06 15:27:34 -0500

    Created /contentsearch route that reroutes search requests to the actual content search application

--- apps/search/src/search/core.clj
+++ apps/search/src/search/core.clj
@@ -3,12 +3,23 @@
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [GET routes]]
             [compojure.route :refer [resources not-found]]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :as response]
+            [clj-http.client :as http]))
 
 (def app
   (-> (routes
        ;; Routes /search to resources/public/search.html
        (GET "/search" [] {:status 301 :headers {"Location" "/search.html"}})
+
+       ;; Routes /contentsearch to the actual content search application
+       (GET "/contentsearch" {query-params :query-params}
+              (let [url "http://localhost:8080/search"
+                    response (http/get url {:query-params query-params})]
+                {:status (:status response)
+                 :headers {"Content-Type" "application/json"}
+                 :body (:body response)}))
+
        (resources "/")
        (not-found "Page not found"))
       wrap-params))

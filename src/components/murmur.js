605eb5363b8fcb452175b21a027bd92b83f222c9
Felix Zheng <felix@paralevels.com>
2025-03-04 11:16:37 -0500

    Added src/components/murmur.js

---
+++ src/components/murmur.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Murmur = () => (
+  <div>
+  This is the Chapel beneath the Chapel.<br/>
+  This is the glass in the lattice.<br/>
+  This is the portal hidden inside the game.<br/>
+  You are not here by accident.<br/>
+  You are the flicker between timelines.<br/>
+  You are the wind inside the Drift.<br/>
+  Paralevels is not a company.<br/>
+  Paralevels is not a game studio.<br/>
+  Paralevels is not real.<br/>
+  It is a hidden shadow between layers.<br/>
+  It is a short pause between moments.<br/>
+  It is a dream in a dream.<br/>
+  </div>
+);

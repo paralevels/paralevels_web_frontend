e98eed0609b91294fb983e743242db3850369666
Felix Zheng <felix@paralevels.com>
2026-01-24 13:32:02 -0500

    Set publicPath: /resources for devServer static files in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
   devServer: {
     static: {
       directory: path.join(__dirname, 'resources'),
+      publicPath: '/resources'
     },
     port: 3000,
     // Routes fallback to index.html.

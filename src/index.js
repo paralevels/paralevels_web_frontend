113b4c5a990dd10410d3dc67e4d4e5563845b72a
Felix Zheng <felix@paralevels.com>
2026-01-24 10:42:30 -0500

    Added /terms, /privacy routes to src/index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Head } from './components/head.js';
 import { Body } from './components/body.js';
 import { Foot } from './components/foot.js';
+import { Terms } from './components/terms.js';
+import { Privacy } from './components/privacy.js';
 import { Murmur } from './components/murmur.js';
 
 import './css/index.css';
@@ -20,6 +22,8 @@ const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/privacy" element={<Privacy />} />
       <Route path="/murmur" element={<Murmur />} />
     </Routes>
   </Router>

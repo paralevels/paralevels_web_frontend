5192606c87f791350fde60a0807b8ea52ad1bfe2
Felix Zheng <felix@paralevels.com>
2026-09-20 19:01:16 -0400

    Updated components/murmur.js

--- src/components/murmur.js
+++ src/components/murmur.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Head } from './head.js';
+import { Foot } from './foot.js';
+
+const Body = () => (
+<div id="body" class="liminal">
+<p>
+This is the Chapel beneath the Chapel.<br/>
+This is the glass in the lattice.<br/>
+This is the portal hidden inside the game.<br/>
+You are not here by accident.<br/>
+You are the flicker between timelines.<br/>
+You are the wind inside the Drift.<br/>
+Paralevels is not a company.<br/>
+Paralevels is not a game studio.<br/>
+Paralevels is not real.<br/>
+It is a quick shadow between layers.<br/>
+It is a short pause between moments.<br/>
+It is a dream in a dream.<br/>
+</p>
+</div>
+);
+
 
 export const Murmur = () => (
-  <div>
-  This is the Chapel beneath the Chapel.<br/>
-  This is the glass in the lattice.<br/>
-  This is the portal hidden inside the game.<br/>
-  You are not here by accident.<br/>
-  You are the flicker between timelines.<br/>
-  You are the wind inside the Drift.<br/>
-  Paralevels is not a company.<br/>
-  Paralevels is not a game studio.<br/>
-  Paralevels is not real.<br/>
-  It is a quick shadow between layers.<br/>
-  It is a short pause between moments.<br/>
-  It is a dream in a dream.<br/>
+  <div id="app">
+    <Head />
+    <Body />
+    <Foot />
   </div>
 );
